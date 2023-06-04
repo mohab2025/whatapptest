6e132caf98494bb8d707b06b630eac7382c44b10
mohab2025 <mohab@gmail.com>
2023-06-04 15:05:57 +0300

    second

--- MVC/controllers/whatsAppWebhook.js
+++ MVC/controllers/whatsAppWebhook.js
@@ -10,9 +10,9 @@ module.exports.verfyWhatsApp = (req, res, next) => {
     res.status(401);
     return next("token not valid");
   }
-  res.status(200).json({ "hub.challenge": challenge });
+  res.status(200).json({ value: challenge });
 };
-// module.exports.getWhatsAppMassege = (req, res, next) => {
+// module.exports.getWhatsAppMassege = (req, res, next) => {s
 //     product
 //       .find({})
 //       .populate("ownerId")
